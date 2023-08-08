39c520b5fa01e3d34031b767252b13ad946e17c8
KEVINBRETS <123332955+KEVINBRETS@users.noreply.github.com>
2023-08-08 17:31:24 +0200

    Maj du cours (les props)

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,29 +1,33 @@
-import { plantList } from '../datas/plantList';
+import { plantList } from '../datas/plantList'
+import CareScale from './CareScale'
 import '../styles/ShoppingList.css'
 
 function ShoppingList() {
-  const categories = plantList.reduce(
-    (acc, plant) =>
-      acc.includes(plant.category) ? acc : acc.concat(plant.category),
-    []
-  );
+	const categories = plantList.reduce(
+		(acc, plant) =>
+			acc.includes(plant.category) ? acc : acc.concat(plant.category),
+		[]
+	)
 
-  return (
-    <div>
-      <ul>
-        {categories.map((category) => (
-          <li key={`${category}`}>{category}</li>
-        ))}
-      </ul>
-      <ul>
-        {plantList.map((plant) => (
-          <li key={`${plant.id}`}>{plant.name}
-          {plant.isBestSale && <span>🔥</span>}
-          {plant.isSpecialOffer && <span className='lmj-sales'> (EN SOLDE)</span>}</li>
-        ))}
-      </ul>
-    </div>
-  );
+	return (
+		<div>
+			<ul>
+				{categories.map((cat) => (
+					<li key={cat}>{cat}</li>
+				))}
+			</ul>
+			<ul className='lmj-plant-list'>
+				{plantList.map((plant) => (
+					<li key={plant.id} className='lmj-plant-item'>
+						{plant.isBestSale && <span>🔥</span>}
+						{plant.name}
+						<CareScale careType='water' scaleValue={plant.water} />
+						<CareScale careType='light' scaleValue={plant.light} />
+					</li>
+				))}
+			</ul>
+		</div>
+	)
 }
 
-export default ShoppingList;
+export default ShoppingList
