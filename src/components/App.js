25c03efb8f03aef000fbc115c8fbfb87cc2846d3
KEVINBRETS <123332955+KEVINBRETS@users.noreply.github.com>
2023-08-10 18:17:47 +0200

    Les useState

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,26 @@
-import Banner from './Banner'
-import logo from '../assets/logo.png'
-// import Cart from './Cart'
-import Footer from './Footer'
-import ShoppingList from './ShoppingList'
+import Banner from './Banner';
+import logo from '../assets/logo.png';
+import Cart from './Cart';
+import Footer from './Footer';
+import ShoppingList from './ShoppingList';
+import '../styles/Layout.css';
 
-function App() {
-	return (
-		<div>
-			<Banner>
-				<img src={logo} alt='La maison jungle' className='lmj-logo' />
-				<h1 className='lmj-title'>La maison jungle</h1>
-			</Banner>
+function App({ cart, updateCart }) {
+  const [cart, updateCart] = useState([]);
 
-			{/* <Cart /> */}
-			<ShoppingList />
-			<Footer />
-		</div>
-	)
+  return (
+    <div>
+      <Banner>
+        <img src={logo} alt="La maison jungle" className="lmj-logo" />
+        <h1 className="lmj-title">La maison jungle</h1>
+      </Banner>
+      <div className="lmj-layout-inner">
+        <Cart cart={cart} updateCart={updateCart} />
+        <ShoppingList cart={cart} updateCart={updateCart} />
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
